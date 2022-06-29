05e491fc2955843849ba884efe221550463b0b3b
Jongmin Kim <jmkim@pukyong.ac.kr>
2022-06-29 16:06:54 +0900

    (WIP)

--- drivers/nand_onfi/nand_onfi.c
+++ drivers/nand_onfi/nand_onfi.c
@@ -752,6 +752,7 @@ bool nand_onfi_wait_until_ready(const nand_onfi_t* const nand, const uint8_t thi
     const uint8_t lun_count = nand->lun_count;
 
     puts("TEST8");
+    while(1) {}
 
     if(ready_other_luns_timeout_ns > 0) {
         for(uint8_t lun_pos = 0; lun_pos < lun_count; ++lun_pos) {
