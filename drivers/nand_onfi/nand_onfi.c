a08074649597e95177babdbc64fc6df8fbe2768f
Jongmin Kim <jmkim@pukyong.ac.kr>
2022-07-15 15:33:16 +0900

    (WIP)

--- drivers/nand_onfi/nand_onfi.c
+++ drivers/nand_onfi/nand_onfi.c
@@ -607,7 +607,7 @@ size_t nand_onfi_read_raw(const nand_onfi_t* const nand, uint16_t* const out_buf
 size_t nand_onfi_read_cycle(const nand_onfi_t* const nand, uint16_t* const out_cycle_data, const uint32_t cycle_read_enable_post_delay_ns, const uint32_t cycle_read_disable_post_delay_ns) {
     print_str("TEST_READY1\r\n");
     const uint16_t a = 0xFFF0;
-    print_byte_hex(0xF);
+    print_u32_hex(cycle_read_disable_post_delay_ns);
     print_str("\r\n");
     print_u32_hex((uint32_t)a);
     print_str("\r\n");
