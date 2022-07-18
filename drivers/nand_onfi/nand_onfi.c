f804fb9d355b3c5231bff2a3b04a3d2f99ec29ea
Jongmin Kim <jmkim@pukyong.ac.kr>
2022-07-18 12:43:42 +0900

    (WIP)

--- drivers/nand_onfi/nand_onfi.c
+++ drivers/nand_onfi/nand_onfi.c
@@ -475,10 +475,12 @@ size_t nand_onfi_write_io(const nand_onfi_t* const nand, const uint16_t* const d
     print_u32_hex((uint32_t)(uintptr_t)nand);
     print_str("\r\n");
     nand_onfi_set_write_enable(nand);
+    print_str("nand_onfi_write_io: TEST1\r\n");
 
     if(cycle_write_enable_post_delay_ns > 0) {
         nand_onfi_wait(cycle_write_enable_post_delay_ns);
     }
+    print_str("nand_onfi_write_io: TEST2\r\n");
 
     if(nand->data_bus_width == 16) {
         gpio_write(nand->params.io15, (*data & NAND_ONFI_MSB15) ? 1 : 0);
@@ -489,6 +491,7 @@ size_t nand_onfi_write_io(const nand_onfi_t* const nand, const uint16_t* const d
         gpio_write(nand->params.io10, (*data & NAND_ONFI_MSB10) ? 1 : 0);
         gpio_write(nand->params.io9, (*data & NAND_ONFI_MSB9) ? 1 : 0);
         gpio_write(nand->params.io8, (*data & NAND_ONFI_MSB8) ? 1 : 0);
+        print_str("nand_onfi_write_io: TEST3\r\n");
     }
 
     gpio_write(nand->params.io7, (*data & NAND_ONFI_MSB7) ? 1 : 0);
@@ -499,12 +502,16 @@ size_t nand_onfi_write_io(const nand_onfi_t* const nand, const uint16_t* const d
     gpio_write(nand->params.io2, (*data & NAND_ONFI_MSB2) ? 1 : 0);
     gpio_write(nand->params.io1, (*data & NAND_ONFI_MSB1) ? 1 : 0);
     gpio_write(nand->params.io0, (*data & NAND_ONFI_MSB0) ? 1 : 0);
+    print_str("nand_onfi_write_io: TEST4\r\n");
 
     nand_onfi_set_write_disable(nand);
+    print_str("nand_onfi_write_io: TEST5\r\n");
 
     if(cycle_write_disable_post_delay_ns > 0) {
         nand_onfi_wait(cycle_write_disable_post_delay_ns);
+        print_str("nand_onfi_write_io: TEST6\r\n");
     }
+    print_str("nand_onfi_write_io: TEST7\r\n");
 
     return 1;
 }
