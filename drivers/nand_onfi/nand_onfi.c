42ea421bd5e6a2e86e732234958bb93c37c0170c
Jongmin Kim <jmkim@pukyong.ac.kr>
2022-07-18 18:53:48 +0900

    (WIP)

--- drivers/nand_onfi/nand_onfi.c
+++ drivers/nand_onfi/nand_onfi.c
@@ -249,6 +249,8 @@ size_t nand_onfi_run_cmd(nand_onfi_t* const nand_onfi, const nand_cmd_t* const c
                             fflush(stdout);
                             nand_set_io_pin_read(nand);
                             print_u32_hex((uint32_t)(uintptr_t)buffer);
+                            print_str("\r\n");
+                            fflush(stdout);
                             return 0;
                             {
                                 rw_size += nand_read_raw(nand, buffer, buffer_size, timings->cycle_rw_enable_post_delay_ns, timings->cycle_rw_disable_post_delay_ns);
