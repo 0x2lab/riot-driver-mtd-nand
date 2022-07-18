371ea1bc263890b2a013b8392ed95c1caa2415c4
Jongmin Kim <jmkim@pukyong.ac.kr>
2022-07-18 18:48:29 +0900

    (WIP)

--- drivers/nand_onfi/nand_onfi.c
+++ drivers/nand_onfi/nand_onfi.c
@@ -235,9 +235,10 @@ size_t nand_onfi_run_cmd(nand_onfi_t* const nand_onfi, const nand_cmd_t* const c
 
                         switch(cycles_type) {
                         case NAND_CMD_TYPE_RAW_WRITE:
+                            print_str("TEST1\r\n");
+                            fflush(stdout);
+                            return 0;
                             {
-                                print_str("TEST1\r\n");
-                                fflush(stdout);
                                 nand_set_io_pin_write(nand);
                                 rw_size += nand_write_raw(nand, buffer, buffer_size, timings->cycle_rw_enable_post_delay_ns, timings->cycle_rw_disable_post_delay_ns);
                             }
@@ -260,6 +261,7 @@ size_t nand_onfi_run_cmd(nand_onfi_t* const nand_onfi, const nand_cmd_t* const c
                         default:
                             print_str("TEST3\r\n");
                             fflush(stdout);
+                            return 0;
                             break;
                         }
                     }
