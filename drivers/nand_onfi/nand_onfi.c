8492a8760f4e469266a63ccec7c47bf14ce4b552
Jongmin Kim <jmkim@pukyong.ac.kr>
2022-07-18 18:39:27 +0900

    (WIP)

--- drivers/nand_onfi/nand_onfi.c
+++ drivers/nand_onfi/nand_onfi.c
@@ -229,6 +229,7 @@ size_t nand_onfi_run_cmd(nand_onfi_t* const nand_onfi, const nand_cmd_t* const c
                     }
 
                     if(buffer != NULL) {
+                        return 0;
                         const size_t raw_remaining_size = *raw_size - *current_raw_offset;
                         buffer_size = (raw_remaining_size > buffer_size) ? buffer_size : raw_remaining_size; /**< Only touch locally, instead of touch the passed param */
 
@@ -240,7 +241,6 @@ size_t nand_onfi_run_cmd(nand_onfi_t* const nand_onfi, const nand_cmd_t* const c
 
                         case NAND_CMD_TYPE_RAW_READ:
                             nand_set_io_pin_read(nand);
-                            return 0;
                             rw_size += nand_read_raw(nand, buffer, buffer_size, timings->cycle_rw_enable_post_delay_ns, timings->cycle_rw_disable_post_delay_ns);
 
                             if(raw->buffer_size != buffer_size) {
