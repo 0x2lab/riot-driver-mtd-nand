f7fdebdd97999a5263c1a8c9e368ba9167f2b664
Jongmin Kim <jmkim@pukyong.ac.kr>
2022-07-18 19:54:54 +0900

    (WIP)

--- drivers/nand_onfi/nand_onfi.c
+++ drivers/nand_onfi/nand_onfi.c
@@ -256,7 +256,6 @@ size_t nand_onfi_run_cmd(nand_onfi_t* const nand_onfi, const nand_cmd_t* const c
                             break;
 
                         case NAND_CMD_TYPE_RAW_READ:
-                            return 0;
                             {
                                 print_u32_hex((uint32_t)(uintptr_t)raw);
                                 print_str("\r\n");
